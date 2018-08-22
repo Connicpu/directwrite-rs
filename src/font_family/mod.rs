34c7116adbcaf6c8c1e1e28c9c1e39a70b62075d
knittenkitten <knittenkitten@pm.me>
2018-08-22 00:17:33 -0500

    implement FontFamily
    probably going to add a lazier version of get_family_names that doesn't return LocalizedStrings at some point

--- src/font_family/mod.rs
+++ src/font_family/mod.rs
@@ -1,3 +1,12 @@
+use enums::{FontStretch, FontStyle, FontWeight};
+use error::DWResult;
+use font::Font;
+use font_list::FontList;
+use localized_strings::LocalizedStrings;
+
+use std::ptr;
+
+use winapi::shared::winerror::SUCCEEDED;
 use winapi::um::dwrite::{IDWriteFontFamily};
 use wio::com::ComPtr;
 
@@ -10,6 +19,45 @@ impl FontFamily{
         FontFamily {
             ptr: ComPtr::from_raw(raw),
         }
+    }
+
+    /// Creates a localized strings object that contains the family names for the font family, indexed by locale name. 
+    pub fn get_family_names(&self) -> DWResult<LocalizedStrings>{
+        unsafe{
+            let mut strings = ptr::null_mut();
+            let hr = self.ptr.GetFamilyNames(&mut strings);
+            if SUCCEEDED(hr){
+                Ok(LocalizedStrings::from_raw(strings))
+            } else {
+                Err(hr.into())
+            }   
+        }     
+    }    
+
+    /// Gets the font that best matches the specified properties.
+    pub fn get_first_matching_font(&self, weight: FontWeight, stretch: FontStretch, style: FontStyle) -> DWResult<Font>{
+        unsafe{
+            let mut font = ptr::null_mut();
+            let hr = self.ptr.GetFirstMatchingFont(weight as u32, stretch as u32, style as u32, &mut font);
+            if SUCCEEDED(hr){
+                Ok(Font::from_raw(font))
+            } else {
+                Err(hr.into())
+            }
+        }   
+    }
+
+    /// Gets a list of fonts in the font family ranked in order of how well they match the specified properties.
+    pub fn get_matching_fonts(&self, weight: FontWeight, stretch: FontStretch, style: FontStyle) -> DWResult<FontList>{
+        unsafe{
+            let mut list = ptr::null_mut();
+            let hr = self.ptr.GetMatchingFonts(weight as u32, stretch as u32, style as u32, &mut list);
+            if SUCCEEDED(hr){
+                Ok(FontList::from_raw(list))
+            } else {
+                Err(hr.into())
+            }  
+        }      
     }
 
     pub unsafe fn get_raw(&self) -> *mut IDWriteFontFamily {
---
+++ src/font_list/mod.rs
@@ -0,0 +1,21 @@
+use winapi::um::dwrite::IDWriteFontList;
+use wio::com::ComPtr;
+
+pub struct FontList {
+    ptr: ComPtr<IDWriteFontList>,
+}
+
+impl FontList {
+    pub unsafe fn from_raw(raw: *mut IDWriteFontList) -> Self {
+        FontList {
+            ptr: ComPtr::from_raw(raw),
+        }
+    }
+
+    pub unsafe fn get_raw(&self) -> *mut IDWriteFontList {
+        self.ptr.as_raw()
+    }
+}
+
+unsafe impl Send for FontList {}
+unsafe impl Sync for FontList {}
